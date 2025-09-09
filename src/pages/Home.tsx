f3d40e5195de8fa44ba89fd958c686bb982c8311
Moeed Rafi <moeedrafi95@gmail.com>
2025-09-09 01:41:56 -0700

    feat: Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,56 @@
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-10">
+      <h1 className="text-3xl font-semibold">Todo List</h1>
+
+      <input
+        type="text"
+        placeholder="todos..."
+        className="w-md px-3 py-2 rounded-lg outline-2 outline-gray-400 focus:outline-2 focus:outline-black"
+      />
+
+      <div className="space-y-4 max-w-md w-full overflow-y-auto mb-5">
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2">
+            <input type="checkbox" />
+            <p>Drink Water</p>
+          </div>
+          <button
+            type="button"
+            className="py-1 px-2 rounded-lg bg-red-600 text-white"
+          >
+            Delete
+          </button>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2">
+            <input type="checkbox" />
+            <p>Drink Water</p>
+          </div>
+          <button
+            type="button"
+            className="py-1 px-2 rounded-lg bg-red-600 text-white"
+          >
+            Delete
+          </button>
+        </div>
+
+        <div className="flex items-center justify-between">
+          <div className="flex gap-2">
+            <input type="checkbox" />
+            <p>Drink Water</p>
+          </div>
+          <button
+            type="button"
+            className="py-1 px-2 rounded-lg bg-red-600 text-white"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
