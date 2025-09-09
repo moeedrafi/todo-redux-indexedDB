e3b1cea03b74e17c58b7511ce39ede24c4f1a598
Moeed Rafi <moeedrafi95@gmail.com>
2025-09-09 01:21:51 -0700

    feat: Login + Signup Page

---
+++ src/components/Card.tsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router";
+
+interface CardProps {
+  header: string;
+  subHeading: string;
+  to?: string;
+  label?: string;
+  children: React.ReactNode;
+}
+
+export const Card = ({
+  children,
+  header,
+  subHeading,
+  label,
+  to,
+}: CardProps) => {
+  return (
+    <section
+      aria-labelledby="form-title"
+      className="bg-white p-4 rounded-lg w-[400px] shadow-md"
+    >
+      {/* HEADER */}
+      <header className="w-full flex flex-col gap-y-3 items-center justify-center">
+        <h1 id="form-title" className="text-3xl font-semibold">
+          🔐 {header}
+        </h1>
+        <p className="text-gray-600 text-sm">{subHeading}</p>
+      </header>
+
+      {/* CONTENT */}
+      <div className="mt-5">{children}</div>
+
+      {/* FOOTER */}
+      {to && label && (
+        <footer className="w-full text-center tracking-tight font-medium">
+          <Link
+            aria-label={`Go to ${label}`}
+            to={to}
+            className="hover:underline underline-offset-2"
+          >
+            {label}
+          </Link>
+        </footer>
+      )}
+    </section>
+  );
+};
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,56 @@
+import { Card } from "../components/Card";
+
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="h-screen flex items-center justify-center">
+      <Card
+        header="Login"
+        subHeading="Welcome Back"
+        to="/signup"
+        label="Don't have an account? Register"
+      >
+        <form className="space-y-6">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="username">Username</label>
+            <input
+              id="username"
+              type="text"
+              placeholder="johndoe.com"
+              className="px-3 py-2 rounded-lg outline-2 outline-gray-400 focus:outline-2 focus:outline-black"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              type="email"
+              autoComplete="on"
+              placeholder="john@doe.com"
+              className="px-3 py-2 rounded-lg outline-2 outline-gray-400 focus:outline-2 focus:outline-black"
+            />
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <label htmlFor="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="********"
+              className="px-3 py-2 rounded-lg outline-2 outline-gray-400 focus:outline-2 focus:outline-black"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="mb-5 w-full p-2 font-semibold rounded-lg bg-black hover:bg-black/80 text-white"
+          >
+            Register
+          </button>
+        </form>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
